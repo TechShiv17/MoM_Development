84daa7a9a61241b6eda715a2ef4c8674116c525e
TechShiv17 <shivam.mathur@nashtechglobal.com>
2024-05-07 16:28:33 +0530

    added email sender feature

--- src/app/main.py
+++ src/app/main.py
@@ -5,15 +5,17 @@ from src.utils.video_downloader import VideoDownloader
 from src.utils.pre_process_data import DataPreProcessing
 from src.utils.summarizer import Summarization
 from src.utils.pdf_generator import PDFGenerator
+from src.utils.email_sender import SMTPEmailSender
 
 
 class VideoToAudioTranscriber:
     url = "https://www.youtube.com/watch?v=sRvLj4D5TuQ"
-    
+
     input_output_path = "../static/"
     video_file_name = "demo_video"
     video_ext = ".mp4"
     audio_file_name = "demo_audio.mp3"
+    pdf_name = "MoM.pdf"
 
     if not os.path.isdir(input_output_path):
         os.mkdir(input_output_path)
@@ -38,4 +40,20 @@ class VideoToAudioTranscriber:
     # To perform summarization of transcribed text
     summary = Summarization.summarization_using_mistral(preprocessedData)
 
-    PDFGenerator.generate_transcript_pdf(summary, f"{input_output_path}/MoM.pdf")
+    # To generate summary PDF
+    PDFGenerator.generate_transcript_pdf(summary, f"{input_output_path}{pdf_name}")
+
+    # Define email configuration
+    subject = "MoM for your Meeting: Demo Meeting"
+    body = "This is the automated Minutes of Meeting (MoM) generated from your meeting"
+    sender_email = "<sender_email_id>"
+    receiver_emails = ["<receiver_email_1, <receiver_email_2>"]
+    password = "<user_generated_token>"
+    filename = f"{input_output_path}{pdf_name}"
+
+    # Create message
+    message = SMTPEmailSender.create_message(sender_email, receiver_emails, subject, body)
+    # Attach file
+    message = SMTPEmailSender.attach_file(message, filename)
+    # Send Email
+    SMTPEmailSender.send_email(sender_email, receiver_emails, password, message)
